efce2c58700940fd976a4e835d746804682bb96d
Asuka109 <asuka109@foxmail.com>
2025-05-12 12:08:30 +0800

    feat: inspectBuffer support to handle uint8array

--- packages/core/src/buffer.ts
+++ packages/core/src/buffer.ts
@@ -1,10 +1,14 @@
+import { scopedBuf } from './utils';
+
 export function arrayBufferToHex(buf: ArrayBuffer) {
   return Array.from(new Uint8Array(buf))
     .map((b) => b.toString(16).padStart(2, '0'))
     .join(' ');
 }
 
-export function inspectBuffer(buf: ArrayBuffer, length = 16) {
-  const hex = arrayBufferToHex(buf.slice(0, length));
+export function inspectBuffer(buf: ArrayBuffer | Uint8Array, length = 16) {
+  const arrBuf = buf instanceof ArrayBuffer ? buf : scopedBuf(buf.buffer);
+  if (!arrBuf) return null;
+  const hex = arrayBufferToHex(arrBuf.slice(0, length));
   return `${hex} +${buf.byteLength - length} bytes`;
 }
